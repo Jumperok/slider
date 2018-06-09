cfb9b64e8648f0c265117e219619ae04ac71843b
Nikita_Suyazov <jumper211212@gmail.com>
2018-06-09 07:59:02 +0300

    https change

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 export const fetchPictures = async () => {
-  let pictures = await fetch('http://www.mocky.io/v2/5b1a50913300007700fb1376')
+  let pictures = await fetch('https://www.mocky.io/v2/5b1a50913300007700fb1376')
     .then(response => response.json())
     .then(data => data.slider)
   return pictures
